a20d0fa8a92b6458482b78c1d9edbb4912550198
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-01-25 18:18:26 -0800

    need to sunc up with master

--- js/forum.js
+++ js/forum.js
@@ -2,17 +2,21 @@
 
 var pastLists = document.getElementById('past-lists'); //DOM ids
 var saveButton = document.getElementById('save-past-list');
-var commentData = [];
+var deleted = document.getElementById('delete');
 var journalEntryData = [];
+var forumData = localStorage.getItem('infoPersist');
+
+if (forumData) { //local storage
+  journalEntryData = JSON.parse(forumData);
+} else {
+  console.log('storage empty yo, initializing!!!!!');
+  localStorage.setItem('infoPersist', JSON.stringify(journalEntryData));
+}
 
 function TripLog(where, details, extra) { //generates info for triplog
   this.where = where;
   this.details = details;
   this.extra = extra;
-  console.log(this.where);
-  console.log(this.details);
-  console.log(this.extra);
-  commentData.push(this);
   this.renderJournal();
 }
 
@@ -30,11 +34,8 @@ var renderAllJournals = function() {
 }
 
 var handleSaveClick = function(event) {
-  console.log('I was clicked. Save these forms');
   event.preventDefault();
-  console.log(event.target);
   var where = event.target.whereTo.value; //form data
-  console.log('after where');
   var details = event.target.detail.value;
   var extra = event.target.extras.value;
 
@@ -45,7 +46,12 @@ var handleSaveClick = function(event) {
   event.target.extras.value= null;
 
   journalEntryData.push(newJournal);
-
   renderAllJournals();
+  localStorage.setItem('infoPersist', JSON.stringify(journalEntryData));
+}
+var handleDeleteClick = function(event) {
+  console.log('deleting local storage!');
+  localStorage.clear();
 }
 pastLists.addEventListener('submit', handleSaveClick); //event listener
+deleted.addEventListener('click', handleDeleteClick);
